5853d8706b8120be95e105a9c1a7e269c1e1ba33
BoyanP <bgpeychoff@gmail.com>
2016-03-12 21:07:34 -0700

    sockets work shoud be able to draw using webSockets

--- sockets.py
+++ sockets.py
@@ -59,21 +59,70 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+
+
+
+#the client stuff was taken from Abram Hindle's webSocket Examples at
+#https://github.com/abramhindle/WebSocketsExamples
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, data):
+        self.queue.put_nowait(data)
+
+    def get(self):
+        return self.queue.get()
+
+
+
+
+myWorld = World() 
+clientList = list()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    #data should have the id and the new value
+	#myWorld.update(entity, data.Keys()[0], data)  
+
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
+    #read the msg from ws and set the variables appropriately in world 
+
+    '''A greenlet function that reads from the websocket'''
+    try:
+        while True:
+            msg = ws.receive()
+            print "WS RECV: %s" % msg
+            if (msg is not None):
+                if(msg == "newConnection"):
+                    client.put(json.dumps(myWorld.world()))
+                    #if it's a new client give them the whole world
+                    #so they can see it 
+
+                else:
+                    packet = json.loads(msg)
+                    for key in packet.keys():
+                        for attribute in packet[key].keys():
+                            myWorld.update(key, attribute, packet[key][attribute])
+
+                    for client in clientList:
+                        client.put(json.dumps(packet))
+                
+            else:
+                break
+    except:
+        '''Done'''    
+
     return None
 
 @sockets.route('/subscribe')
@@ -81,8 +130,19 @@ def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
-
+    client = Client()
+    clientList.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print "WS Error %s" % e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)		
 
 def flask_post_json():
     '''Ah the joys of frameworks! They do so much work for you
@@ -97,25 +157,34 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    jsonData = flask_post_json()
+    if request.method == 'PUT':
+        myWorld.set(entity,jsonData)
+
+    if request.method == "POST":
+        print jsonData
+        for key in jsonData.keys():
+            myWorld.update(entity,key,jsonData[key])
+
+    result = myWorld.get(entity)
+    return json.dumps(result)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
